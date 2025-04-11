d4410a14f11752cbb935d729a0ff28de570d0020
Clément De Roberti <cderoberti@gmail.com>
2025-04-11 15:39:05 +0200

    glabal : ajout outil analytics vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 // app/page.tsx
 "use client";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import Hero from "@/components/Hero";
 import APropos from "@/components/APropos";
 import Citation from "@/components/Citation";
@@ -11,6 +13,9 @@ import Stats from "@/components/Stats";
 export default function Home() {
   return (
     <main className="bg-[#fdfdfd] text-[#313131]">
+      <div className="bg-[#fdfdfd] text-[#313131]">
+        <Analytics />
+      </div>
       <Hero />
       <APropos />
       <Citation />
